f04f37dc9896d21f0c4807fb195eef256f3d7476
Kevin <k.a.higuera95@gmail.com>
2014-10-19 15:26:20 -0700

    first working prototype. No connector functionality

--- src/main.cpp
+++ src/main.cpp
@@ -3,25 +3,62 @@
 #include <errno.h>
 #include <sys/types.h>
 #include <sys/wait.h>
+#include <string.h>
 #include <string>
 #include <iostream>
+#include <cstdlib>
 
 using namespace std;
-
-int main(int  argc, char *const argv[])
+#define DELIMS " \t\r\n"
+#define MAX 1000
+int main()
 {
-	int pid=fork();
-	if(pid==0)
+	char line[MAX];
+	char *cmd=NULL;
+	while(1)
 	{
-		cout << "child" <<endl;
-		
-		execvp("ls",argv);
+		//print out prompt and wait for user input
+		cout << "$ ";
+		if(!fgets(line, MAX, stdin))
+			break;
+		//parse line for first cmd and see if it is exit
+		cmd=strtok(line, DELIMS);
+
+		if(strcmp(cmd,"exit")==0)
+			exit(0);
+		//if not exit attempt to execute cmd
+		else{
+			int id=fork();
+			if(id==0)
+			{
+				char* argv[50];
+				argv[0]=new char[6];
+				strcpy(argv[0],cmd);
+				cmd=strtok(NULL,DELIMS);
+				unsigned int i=0;
+				while(cmd!=NULL)
+				{
+					strcpy(argv[i+1],cmd);
+					cmd=strtok(NULL,DELIMS);
+					++i;
+				}
+				argv[i+1]=0;
+				int pid =execvp(argv[0],argv);
+				if(pid==-1)
+					perror("execvp");
+				//be sure to exit if execvp failed
+				exit(1);
+			}
+			else if(id==-1)
+			{
+				perror("fork");
+				//be sure to exit if fork failed
+				exit(1);
+			}
+			else
+				wait(NULL);
+		}
 	}
-	else
-	{
-		wait(NULL);
-		cout << "parent"<<endl;
-	}
+
 	return 0;
 }
-
