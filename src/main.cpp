381b2069db03aceec96ca98364abddd9afae4e4e
Kevin <k.a.higuera95@gmail.com>
2014-10-21 16:29:13 -0700

    rshell working at half according to rubric

--- src/main.cpp
+++ src/main.cpp
@@ -9,35 +9,48 @@
 #include <cstdlib>
 
 using namespace std;
-#define DELIMS " \t\r\n"
-#define MAX 1000
+#define DELIMS " \r\t\n\""
+#define MAX 1024
 int main()
 {
 	char line[MAX];
-	char *cmd=NULL;
+	char *cmd=NULL;		//cmd is used for the parsed string, first parse is the cmd
+	char user[256]={0};
+	char host[256]={0};
+	bool hasQuote=false;
+	getlogin_r(user,sizeof(user)-1);
+	gethostname(host,sizeof(host)-1);
 	while(1)
 	{
 		//print out prompt and wait for user input
-		cout << "$ ";
-		if(!fgets(line, MAX, stdin))
-			break;
+
+		cout<<user<<"@"<<host<< "$ ";
+		fgets(line, MAX, stdin);
+		for(unsigned j=0;j<MAX;++j)
+		{
+			if(line[j]=='\"')
+				hasQuote=true;
+			if(!hasQuote&&line[j]=='#')
+				line[j]='\0';
+		}
 		//parse line for first cmd and see if it is exit
 		cmd=strtok(line, DELIMS);
-
 		if(strcmp(cmd,"exit")==0)
 			exit(0);
 		//if not exit attempt to execute cmd
-		else{
+		else
+		{
 			int id=fork();
 			if(id==0)
 			{
-				char* argv[50];
-				argv[0]=new char[6];
+				char* argv[MAX];
+				argv[0]=new char[50];
 				strcpy(argv[0],cmd);
 				cmd=strtok(NULL,DELIMS);
 				unsigned int i=0;
 				while(cmd!=NULL)
 				{
+					argv[i+1]=new char[50];
 					strcpy(argv[i+1],cmd);
 					cmd=strtok(NULL,DELIMS);
 					++i;
