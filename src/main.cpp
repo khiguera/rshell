0aabb9ce9604c6507fea51342bf8a72d8c4754a1
Kevin <k.a.higuera95@gmail.com>
2014-11-20 23:25:09 -0800

    possible final commit for hw2

--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,73 @@
 using namespace std;
 #define DELIMS " \r\t\n\""
 #define MAX 1024
+void piping(char in[],int count,int cmdnum)
+{
+	unsigned pos=0;
+	int pipefd[2];
+	int fd;
+	char *str=NULL;
+	char leftPart[MAX];
+	char rightPart[MAX];
+	cerr<<in<<endl;
+	for(unsigned i=0;in[i]!='|';++i)
+	{
+		leftPart[i]=in[i];
+		++pos;
+	}
+	++pos;
+	for(unsigned i=0;in[i]!='\0';++i)
+	{
+		rightPart[i]=in[pos];
+		++pos;
+	}
+	cerr<<leftPart<<endl;
+	cerr<<rightPart<<endl;
+	if(cmdnum!=count)
+	{
+		fd=pipe(pipefd);
+		if(fd==-1)
+		{
+			perror("pipe");
+			exit(1);
+		}
+	}
+	int id=fork();
+	if(id==0)//in child process
+	{
+		str=strtok(leftPart,DELIMS);
+		char* argv[MAX];
+		argv[0]=new char[50];
+		strcpy(argv[0],str);
+		str=strtok(NULL,DELIMS);
+		unsigned int i=0;
+		while(str!=NULL)//tokenize and set arguments for execvp
+		{
+			argv[i+1]=new char[50];
+			strcpy(argv[i+1],str);
+			str=strtok(NULL,DELIMS);
+			++i;
+		}
+		argv[i+1]='\0';//add null terminating character
+			if(cmdnum==0)
+			{
+				close(1);
+				dup(fd);
+			}
+		piping(rightPart,count,++cmdnum);
+		int pid =execvp(argv[0],argv);
+		if(pid==-1)
+		{//exit if execvp failed
+			perror("execvp");
+			exit(1);
+		}
+		if(wait(NULL)==-1)
+		{
+			perror("wait");
+			exit(1);
+		}
+	}			
+}
 int main()
 {
 	char line[MAX];		//line is used for input
@@ -18,18 +85,19 @@ int main()
 	char user[256];		
 	char host[256];
 	bool hasQuote=false;
+	int pipeCnt=0;
+	bool hasPipe=false;
 	int log=getlogin_r(user,sizeof(user)-1);	//login
 	int hos=gethostname(host,sizeof(host)-1);	//hostname
 	if(log!=0)
-		perror("getlogin");
+	perror("getlogin");
 	if(hos!=0)
-		perror("gethostname");
+	perror("gethostname");
 	while(1)
 	{
 		//print out prompt and wait for user input
 		cout<<user<<"@"<<host<< "$ ";
 		fgets(line, MAX, stdin);
-
 		//check for quote or comment and set null terminating accordingly
 		for(unsigned j=0;j<MAX;++j)
 		{
@@ -37,12 +105,21 @@ int main()
 				hasQuote=true;
 			if(!hasQuote&&line[j]=='#')
 				line[j]='\0';
+			if(line[j]=='|')
+			{
+				hasPipe=true;
+				++pipeCnt;
+			}
 		}
-
+		if(hasPipe)
+		{
+			piping(line,pipeCnt,0);
+			hasPipe=false;
+		}
 		//parse line for first cmd and see if it is exit
 		cmd=strtok(line, DELIMS);
 		if(strcmp(cmd,"exit")==0)
-			exit(0);
+				exit(0);
 		//if not exit attempt to execute cmd
 		else
 		{
@@ -65,7 +142,7 @@ int main()
 				int pid =execvp(argv[0],argv);
 				if(pid==-1) //exit if execvp failed
 					perror("execvp");
-				exit(1);
+					exit(1);
 			}
 			else if(id==-1) //exit if fork failed
 			{
@@ -75,7 +152,7 @@ int main()
 			else //in parent process
 				wait(NULL);
 		}
+		
 	}
-
 	return 0;
 }
